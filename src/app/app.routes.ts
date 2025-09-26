aa15cb424a3ffcc0a7e48433f5a45f2e8a758196
Araden14 <arnaud-danselme@hotmail.com>
2025-09-26 17:44:35 +0200

    fix : remove import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 import { adminGuard } from './core/guards/admin.guard';
-import { RegisterComponent } from './features/auth/components/register/register.component';
 
 export const routes: Routes = [
   {
